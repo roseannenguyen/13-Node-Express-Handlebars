b6fc52e92d4fbf6090ce5db48daac1dc777fd15f
Roseanne Nguyen <rtnguye1@gmail.com>
2021-02-20 21:34:07 -0700

    created folder directories and set up the connection and server files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Bacon Burger', false);
